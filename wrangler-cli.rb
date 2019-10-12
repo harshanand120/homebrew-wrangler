4a78036a3193bc3a70384ef871a5ac94dc7a9700
Barend <11941174+bvanrijn@users.noreply.github.com>
2019-10-12 15:10:52 +0200

    wrangler v1.5.0-rc.0

--- wrangler-cli.rb
+++ wrangler-cli.rb
@@ -5,8 +5,8 @@ class WranglerCli < Formula
   sha256 "19ecfd7b024bc294eed5154a58cc2bdcb34959472ad003c3529db1dadf01566a"
 
   devel do
-    url "https://github.com/cloudflare/wrangler/releases/download/v1.4.0-rc.7/wrangler-v1.4.0-rc.7-x86_64-apple-darwin.tar.gz"
-    sha256 "b7639b25270863cc1ae038c43e774b6f9c045b5aeb9f6a9e74cfe5b94e4fca45"
+    url "https://github.com/cloudflare/wrangler/releases/download/v1.5.0-rc.0/wrangler-v1.5.0-rc.0-x86_64-apple-darwin.tar.gz"
+    sha256 "f796b306253be89a759216d85fab55456e2eeae2f542d6b8275418964887e00e"
   end
 
   def install
