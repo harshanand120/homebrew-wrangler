ce365b222d67065ae056fa82c776c5a1a359b2cd
Barend <11941174+bvanrijn@users.noreply.github.com>
2019-11-11 21:30:19 +0100

    wrangler 1.6.0-rc.0

--- wrangler-cli.rb
+++ wrangler-cli.rb
@@ -5,8 +5,8 @@ class WranglerCli < Formula
   sha256 "68f51de6725952c42e29bdcf375a776adbb42fb6180d9cd25c0c0002af4329c5"
 
   devel do
-    url "https://github.com/cloudflare/wrangler/releases/download/v1.5.0-rc.0/wrangler-v1.5.0-rc.0-x86_64-apple-darwin.tar.gz"
-    sha256 "f796b306253be89a759216d85fab55456e2eeae2f542d6b8275418964887e00e"
+    url "https://github.com/cloudflare/wrangler/releases/download/v1.6.0-rc.0/wrangler-v1.6.0-rc.0-x86_64-apple-darwin.tar.gz"
+    sha256 "1894f5cf2061b8a4e96f51261dbd07aefc21cf3f747a3b68e01448d091b41433"
   end
 
   def install
@@ -18,6 +18,6 @@ class WranglerCli < Formula
     version_output = shell_output("#{bin}/wrangler --version")
 
     assert_includes version_output, "1.5.0" unless build.devel?
-    assert_includes version_output, "1.5.0-rc.0" if build.devel?
+    assert_includes version_output, "1.6.0-rc.0" if build.devel?
   end
 end
