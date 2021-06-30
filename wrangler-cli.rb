b47e6524879499d3921e83730cc5ef3b4cea2225
Harsh Anand <harshanand120@gmail.com>
2021-07-01 02:09:06 +0545

    v1.17.0

--- wrangler-cli.rb
+++ wrangler-cli.rb
@@ -1,8 +1,8 @@
 class WranglerCli < Formula
   desc "Wrangle your cloudflare workers"
   homepage "https://workers.cloudflare.com/"
-  url "https://github.com/cloudflare/wrangler/releases/download/v1.16.1/wrangler-v1.16.1-x86_64-apple-darwin.tar.gz"
-  sha256 "cc404a556a15fbd555bfa0f4e28ac6bc3f7eb8d141c34845239bc0ec6af1fec8"
+  url "https://github.com/cloudflare/wrangler/releases/download/v1.17.0/wrangler-v1.17.0-x86_64-apple-darwin.tar.gz"
+#   sha256 "cc404a556a15fbd555bfa0f4e28ac6bc3f7eb8d141c34845239bc0ec6af1fec8"
 
   # head do
   #   url "https://github.com/cloudflare/wrangler/releases/download/v1.11.0-rc.0/wrangler-v1.11.0-rc.0-x86_64-apple-darwin.tar.gz"
@@ -17,7 +17,7 @@ class WranglerCli < Formula
     # TODO: Find a better test that won't break on every release.
     version_output = shell_output("#{bin}/wrangler --version")
 
-    assert_includes version_output, "1.16.1" unless build.head?
+    assert_includes version_output, "1.17.0" unless build.head?
     # assert_includes version_output, "1.11.0-rc.0" if build.head?
   end
 end
